f9b2c239f4c482fb99d4396da975988324925cc6
Ashikurahman23 <sm.ashikurahman@gmail.com>
2025-09-02 22:59:47 +0600

    all lesson fetching done

--- home.js
+++ home.js
@@ -25,7 +25,6 @@ lessonBtn.addEventListener('click', function (e) {
       .then(lessons => {
         const data = lessons.data;
         console.log(data);
-
         data.forEach(lesson => {
           const createCard = document.createElement('div');
           cardContainer.classList.add(
@@ -35,9 +34,7 @@ lessonBtn.addEventListener('click', function (e) {
             'lg:grid-cols-3',
             'gap-6'
           );
-
           createCard.innerHTML = `
-  
         <div class="bg-white p-6 text-black rounded-xl">
         <div class="space-y-4 ">
         <h4 class="text-3xl font-bold text-sky-600">${
@@ -54,12 +51,208 @@ lessonBtn.addEventListener('click', function (e) {
           lesson.word
         }')"></i>
       </div>
-      </div>
-    
-    
+      </div>  
     `;
           cardContainer.appendChild(createCard);
         });
       });
+  }
+  if (e.target.innerText === 'Lesson - 2') {
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = '';
+
+    fetch('https://openapi.programming-hero.com/api/level/2/')
+      .then(response => response.json())
+      .then(lessons => {
+        const data = lessons.data;
+        console.log(data);
+        data.forEach(lesson => {
+          const createCard = document.createElement('div');
+          cardContainer.classList.add(
+            'grid',
+            'grid-cols-1',
+            'md:grid-cols-2',
+            'lg:grid-cols-3',
+            'gap-6'
+          );
+          createCard.innerHTML = `
+        <div class="bg-white p-6 text-black rounded-xl">
+        <div class="space-y-4 ">
+        <h4 class="text-3xl font-bold text-sky-600">${
+          lesson.word || 'Now Word'
+        }</h4>
+        <p class="text-xl font-semibold">Meaning/ Pronounciation</p>
+        <h3 class="text-2xl font-bold text-blue-800">"${
+          lesson.meaning || 'অর্থ পাওয়া যায় নি'
+        }"</h3>
+      </div>
+      <div class="flex justify-between mt-16 text-2xl text-gray-700">
+        <i class="ri-information-2-fill bg-blue-500/10 p-2 rounded-md"></i>
+        <i class="ri-volume-up-fill bg-blue-500/10 p-2 rounded-md" onclick="pronounceWord('${
+          lesson.word
+        }')"></i>
+      </div>
+      </div>  
+    `;
+          cardContainer.appendChild(createCard);
+        });
+      });
+  }
+  if (e.target.innerText === 'Lesson - 3') {
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = '';
+
+    fetch('https://openapi.programming-hero.com/api/level/3/')
+      .then(response => response.json())
+      .then(lessons => {
+        const data = lessons.data;
+        console.log(data);
+        data.forEach(lesson => {
+          const createCard = document.createElement('div');
+          cardContainer.classList.add(
+            'grid',
+            'grid-cols-1',
+            'md:grid-cols-2',
+            'lg:grid-cols-3',
+            'gap-6'
+          );
+          createCard.innerHTML = `
+        <div class="bg-white p-6 text-black rounded-xl">
+        <div class="space-y-4 ">
+        <h4 class="text-3xl font-bold text-sky-600">${
+          lesson.word || 'Now Word'
+        }</h4>
+        <p class="text-xl font-semibold">Meaning/ Pronounciation</p>
+        <h3 class="text-2xl font-bold text-blue-800">"${
+          lesson.meaning || 'অর্থ পাওয়া যায় নি'
+        }"</h3>
+      </div>
+      <div class="flex justify-between mt-16 text-2xl text-gray-700">
+        <i class="ri-information-2-fill bg-blue-500/10 p-2 rounded-md"></i>
+        <i class="ri-volume-up-fill bg-blue-500/10 p-2 rounded-md" onclick="pronounceWord('${
+          lesson.word
+        }')"></i>
+      </div>
+      </div>  
+    `;
+          cardContainer.appendChild(createCard);
+        });
+      });
+  }
+  if (e.target.innerText === 'Lesson - 4') {
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = '';
+    const createCard = document.createElement('div');
+    createCard.classList.add(
+      'flex',
+      'flex-col',
+      'justify-center',
+      'items-center'
+    );
+    createCard.innerHTML = `
+          <img src="./assets/alert-error.png" class="my-6"/>
+        <p>এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h3 class="text-4xl font-semibold mt-6">নেক্সট Lesson এ যান</h3>
+    `;
+    cardContainer.appendChild(createCard);
+  }
+  if (e.target.innerText === 'Lesson - 5') {
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = '';
+
+    fetch('https://openapi.programming-hero.com/api/level/5/')
+      .then(response => response.json())
+      .then(lessons => {
+        const data = lessons.data;
+        console.log(data);
+        data.forEach(lesson => {
+          const createCard = document.createElement('div');
+          cardContainer.classList.add(
+            'grid',
+            'grid-cols-1',
+            'md:grid-cols-2',
+            'lg:grid-cols-3',
+            'gap-6'
+          );
+          createCard.innerHTML = `
+        <div class="bg-white p-6 text-black rounded-xl">
+        <div class="space-y-4 ">
+        <h4 class="text-3xl font-bold text-sky-600">${
+          lesson.word || 'Now Word'
+        }</h4>
+        <p class="text-xl font-semibold">Meaning/ Pronounciation</p>
+        <h3 class="text-2xl font-bold text-blue-800">"${
+          lesson.meaning || 'অর্থ পাওয়া যায় নি'
+        }"</h3>
+      </div>
+      <div class="flex justify-between mt-16 text-2xl text-gray-700">
+        <i class="ri-information-2-fill bg-blue-500/10 p-2 rounded-md"></i>
+        <i class="ri-volume-up-fill bg-blue-500/10 p-2 rounded-md" onclick="pronounceWord('${
+          lesson.word
+        }')"></i>
+      </div>
+      </div>  
+    `;
+          cardContainer.appendChild(createCard);
+        });
+      });
+  }
+  if (e.target.innerText === 'Lesson - 6') {
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = '';
+
+    fetch('https://openapi.programming-hero.com/api/level/6/')
+      .then(response => response.json())
+      .then(lessons => {
+        const data = lessons.data;
+        console.log(data);
+        data.forEach(lesson => {
+          const createCard = document.createElement('div');
+          cardContainer.classList.add(
+            'grid',
+            'grid-cols-1',
+            'md:grid-cols-2',
+            'lg:grid-cols-3',
+            'gap-6'
+          );
+          createCard.innerHTML = `
+        <div class="bg-white p-6 text-black rounded-xl">
+        <div class="space-y-4 ">
+        <h4 class="text-3xl font-bold text-sky-600">${
+          lesson.word || 'Now Word'
+        }</h4>
+        <p class="text-xl font-semibold">Meaning/ Pronounciation</p>
+        <h3 class="text-2xl font-bold text-blue-800">"${
+          lesson.meaning || 'অর্থ পাওয়া যায় নি'
+        }"</h3>
+      </div>
+      <div class="flex justify-between mt-16 text-2xl text-gray-700">
+        <i class="ri-information-2-fill bg-blue-500/10 p-2 rounded-md"></i>
+        <i class="ri-volume-up-fill bg-blue-500/10 p-2 rounded-md" onclick="pronounceWord('${
+          lesson.word
+        }')"></i>
+      </div>
+      </div>  
+    `;
+          cardContainer.appendChild(createCard);
+        });
+      });
+  }
+  if (e.target.innerText === 'Lesson - 7') {
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = '';
+    const createCard = document.createElement('div');
+    createCard.classList.add(
+      'flex',
+      'flex-col',
+      'justify-center',
+      'items-center'
+    );
+    createCard.innerHTML = `
+          <img src="./assets/alert-error.png" class="my-6"/>
+        <p>এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h3 class="text-4xl font-semibold mt-6">নেক্সট Lesson এ যান</h3>
+    `;
+    cardContainer.appendChild(createCard);
   }
 });
